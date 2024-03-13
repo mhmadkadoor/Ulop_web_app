c9490ac40eba31f48caf1272a5d53944433a89c9
mhmadkadoor <mhmadkadoor2004@gmail.com>
2024-03-13 17:26:52 +0300

    Add Post model and migrations

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posted_images/%Y/%m/%d/')
+    date_posted = models.DateTimeField(auto_now_add=True)
+    sender_id = models.IntegerField()
+    sender_name = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
+    
