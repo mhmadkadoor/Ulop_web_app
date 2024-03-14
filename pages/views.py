80bc7de9c4ba875fab12cfbe04122f78e624a631
mhmadkadoor <mhmadkadoor2004@gmail.com>
2024-03-14 10:59:33 +0300

    Add new pages and update styles

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
 
 # Create your views here.
 
 def home (request):
     return render(request, 'pages/htmls/home.html')
+def about (request):
+    return render(request, 'pages/htmls/about.html')
+def courses (request):
+    return render(request, 'pages/htmls/courses.html')
