c4876eea17cc8cbb4b19730a711fb258293cd0e3
mhmadkadoor <mhmadkadoor2004@gmail.com>
2024-03-14 11:17:55 +0300

    Add posts app and URLs

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def posts(request):
+    return render(request, 'posts/posts.html')
+def post(request):
+    return render(request, 'posts/post.html')
