e5825222f0d4fc309b2fb0830e53b197251ab71e
MHD HADI SAID ALKADDOUR <69122415+mhmadkadoor@users.noreply.github.com>
2024-03-27 17:24:36 +0300

    Add new post editing functionality

--- pages/urls.py
+++ pages/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('post/edit/<int:post_id>/', views.edit_post, name='edit_post'),
 ]
